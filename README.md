44c0401a2546eb09f9e499a0d7f0ca82835ef4f5
ddiu8081 <ddiu8081@gmail.com>
2022-01-11 16:36:05 +0800

    feat: first commit & site header

---
+++ README.md
@@ -0,0 +1,5 @@
+# voice.ddiu.site
+
+A landing page of voice server.
+
+[Website](https://voice.ddiu.site/).
---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Icon from '@iconify/svelte'
+</script>
+
+<main class="container max-w-prose mx-auto mt-16 mb-10">
+  <header class="py-4">
+    <Icon class="text-6xl" icon="twemoji:radio" />
+    <h1 class="text-4xl font-semibold mt-4">Diu Voice Server</h1>
+    <p class="opacity-50 mt-2">Another Teamspeak server in China.</p>
+  </header>
+  <article>
+    <div class="my-4">
+      <h2 class="uppercase opacity-50 text-sm">Server Name</h2>
+      <div class="flex items-center justify-between px-4 py-3 -mx-2 my-3 bg-gray-100 rounded-lg">
+        <span class="text-xl font-semibold font-mono text-gray-600">voice.ddiu.site</span>
+        <Icon class="text-3xl opacity-80" icon="twemoji:flag-for-flag-china" />
+      </div>
+    </div>
+  </article>
+  <footer class="text-sm opacity-50 mt-12">
+    2021 © Diu 
+  </footer>
+</main>
